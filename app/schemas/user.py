66791ff346e5ccbb2c0ec5b8cd3684ecd8083168
Abd217391 <ahmad@visnext.net>
2025-12-30 16:49:06 +0500

    feat: update user profile endpoint

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -20,3 +20,27 @@ class ChangePassword(BaseModel):
     email: EmailStr
     old_password: str
     new_password: str 
+
+from typing import Optional
+
+class UpdateProfile(BaseModel):
+    firstname: Optional[str] = None
+    lastname: Optional[str] = None
+    
+
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
+
+
+from pydantic import BaseModel
+from datetime import datetime
+
+class UserOut(BaseModel):
+    id: int
+    firstname: str
+    lastname: str
+    email: str
+    createdat: datetime   
+    class Config:
+        from_attributes = True  
